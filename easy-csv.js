63cbdadabc68356c80da33e871cf5e147592f382
Jon Christensen <JCodesMN@gmail.com>
2017-05-31 19:14:39 -0500

    logging config in run()

--- easy-csv.js
+++ easy-csv.js
@@ -1,9 +1,86 @@
+// https://github.com/jcodesmn/google-apps-script-cheat-sheet 
+
+var config = importConfiguration("https://raw.githubusercontent.com/jcodesmn/easy-csv/master/apple-school-manager.json");
+
+// json
+
+function jsonFromUrl(url) {
+  var rsp  = UrlFetchApp.fetch(url);
+  var data = rsp.getContentText();
+  var json = JSON.parse(data);
+  return json;
+} 
+
+function jsonFromFile(file) {
+  var data = file.getBlob().getDataAsString();
+  var json = JSON.parse(data);
+  return json;
+} 
+
+function importConfiguration(scriptConfig) {
+  var regExp = new RegExp("^(http|https)://");
+  var test   = regExp.test(scriptConfig);
+  var json;
+  if (test) {
+    json = jsonFromUrl(scriptConfig); 
+    return json;
+  } else {
+    var file = findFileAtPath(scriptConfig); 
+    json = jsonFromFile(file); 
+    return json;
+  }
+}
+
+// array of objects
+
+function numCol(number) {
+  var num = number - 1, chr;
+  if (num <= 25) {
+    chr = String.fromCharCode(97 + num).toUpperCase();
+    return chr;
+  } else if (num >= 26 && num <= 51) {
+    num -= 26;
+    chr = String.fromCharCode(97 + num).toUpperCase();
+    return "A" + chr;
+  } else if (num >= 52 && num <= 77) {
+    num -= 52;
+    chr = String.fromCharCode(97 + num).toUpperCase();
+    return "B" + chr;
+  } else if (num >= 78 && num <= 103) {
+    num -= 78;
+    chr = String.fromCharCode(97 + num).toUpperCase();
+    return "C" + chr;
+  }
+}
+
+function headerVal(rangeObj){
+  var vals = rangeObj.getValues();
+  var arr  = [];
+  for (var i = 0; i < vals[0].length; i++) {
+    var val = vals[0][i];
+    arr.push(val);
+  } 
+  return arr;
+}
+
+function arrObjFromSheet(sheetObj, hRow){
+  var lColNum = sheetObj.getLastColumn();
+  var lColABC = numCol(lColNum);
+  var lRow    = sheetObj.getLastRow();
+  var hRange  = sheetObj.getRange("A" + hRow + ":" + lColABC + hRow);
+  var headers = headerVal(hRange);
+  var vRange  = sheetObj.getRange("A" + (hRow +1) + ":" + lColABC + lRow);
+  return valByRow(vRange, headers);
+}
+
+// folders
+
 function createVerifyPath(path) {
   var arr = path.split('/');
   var fldr;
   for (i = 0; i < arr.length; i++) {
-    if (i == 0) {
-      var fi = DriveApp.getRootFolder().getFoldersByName(arr[i]);
+    var fi = DriveApp.getRootFolder().getFoldersByName(arr[i]);
+    if (i === 0) {
       if(!(fi.hasNext())) {
         DriveApp.createFolder(arr[i]);
         fi = DriveApp.getFoldersByName(arr[i]);
@@ -20,3 +97,11 @@ function createVerifyPath(path) {
   } 
   return fldr;
 }
+
+function run() {
+
+  Logger.log(config);
+
+}
+
+
